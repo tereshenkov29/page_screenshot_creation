7b9b05a1feadc39738e47ffe7a459da849e1f771
Yakov Tereshenkov <tereshenkov@gmail.com>
2025-04-17 03:33:40 +0300

    update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from screenshot_playwright import take_fullpage_screenshot
 from upload_to_shared_drive import upload_all_screenshots_to_shared_drive
 import threading
 import time
+import os
 
 app = Flask(__name__)
 
@@ -97,4 +98,4 @@ def status():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
